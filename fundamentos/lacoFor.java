f8b220e51ca8e1e81e7d702d1592ea7c255ac6f0
José Carlos dos Anjos <79722929+jcanjosjr@users.noreply.github.com>
2021-06-27 18:30:27 -0300

    Delete fundamentos directory

--- fundamentos/lacoFor.java
+++
@@ -1,9 +0,0 @@
-package usuario;
-
-public class lacoFor {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("I = " + i);
-        }
-    }
-}
